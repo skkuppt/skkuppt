9be2c3e71ab679770ecb02867711fd80230b4d15
ãJunChangM <jsjs1209@g.skku.edu>
2023-11-07 00:03:36 +0900

    Feat:login and register and link db

---
+++ pages/signup.jsx
@@ -0,0 +1,45 @@
+import Layout from '../components/layout'
+import { getCookie } from 'cookies-next';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+export default function SignupPage( {username} ) {
+    const router = useRouter()
+    const { msg } = router.query
+    return (
+        <Layout pageTitle="Signup">
+            <Link href="/">Home</Link><br/>
+            {msg ?
+                <h3 className="red">{msg}</h3>
+            :
+                <></>
+            }
+            <h2>Sign up Test Page</h2>
+            <form action='/api/signup' method='POST'>
+                <input minLength="3" name="username" id="username" type="text" placeholder='username' required></input><br/>
+                <input minLength="5" name="password" id="password" type="password" placeholder='password' required></input><br/>
+                <input minLength="5" name="passwordagain" id="passwordagain" type="password" placeholder='password again' required></input><br/>
+                <input type="submit" value="Signup"/><br/>
+                <br/>
+
+                <input minLength="5" name="emailVerification" id="emailVerification" type="emailVerification" placeholder='emailVerification again' required></input>
+                <input type="submit" value="Verfity"/>
+            </form>
+        </Layout>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const req = context.req
+    const res = context.res
+    var username = getCookie('username', { req, res });
+    if (username != undefined){
+        return {
+            redirect: {
+                permanent: false,
+                destination: "/"
+            }
+        }
+    }
+    return { props: {username:false} };
+};
