96dbfd6ded8557d4289d3049ed67872d77dc37c9
Worldeditory <shh9904@gmail.com>
2023-11-16 15:24:39 +0900

    PPTmaker test code and complete the main script

--- utils/PPTmaker.py
+++ utils/PPTmaker.py
@@ -1,4 +1,3 @@
-import openai
 from openai import OpenAI
 
 
@@ -13,22 +12,34 @@ def gpt_pptmaker(topic, details, apikey):
     {details}
     In the structure of:
     Slide 1:
+    Title:
+    Content:
     Slide 2:
+    Title:
+    Content:
     ...  
-    Slide n:  
+    Slide n:
+    Title:
+    Content:
+    Only write the title of the slides and its content. 
     """
 
     completion = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+        model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are making ppt for the class, skilled in making index and its belongings."},
+            {"role": "system", "content": "You are making ppt for the class, skilled in making index and its belongings.\
+              showing only the title and content of the slides without other flowery words or summary."},
             {"role": "user", "content": prompt}
         ],
-
     )
+
     # get the response content only
-    print(completion.choices[0].message)
-    return completion.choices[0].message
+    print(completion.choices[0].message.content)
+    slides = completion.choices[0].message.content.split("Slide")
+    slides = slides[1:]
+
+    slides = ["Slide" + slide.strip() for slide in slides]
+    return slides
 
 
 
@@ -36,5 +47,5 @@ if __name__ == '__main__':
     topic = "What is the meaning of life?"
     details = "The meaning of life is to be happy and useful."
     apikey = ""
-    gpt_pptmaker(topic, details, apikey)
+    print(gpt_pptmaker(topic, details, apikey))
     
