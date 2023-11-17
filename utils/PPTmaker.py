77992377dc33d796098774b58b6855d4a9d31969
Sunhong Hwang <39299270+Worldeditory@users.noreply.github.com>
2023-11-17 19:25:41 +0900

    Algorithm (#2)
    * initial script(currently not working)
    
    * PPT_maker_prototype
    
    * PPTmaker test code and complete the main script
    
    * add api_key
    
    * update prompt

---
+++ utils/PPTmaker.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+
+
+def gpt_pptmaker(topic, details, apikey):
+    client = OpenAI(
+        api_key=apikey,
+    )
+
+    # prompt
+    prompt = f"""
+    make a powerpoint presentation about {topic} with the following details:
+    {details}
+    In the structure of:
+    Slide 1:
+    Title:
+    Content:
+    Slide 2:
+    Title:
+    Content:
+    ...  
+    Slide n:
+    Title:
+    Content:
+    Only write the title of the slides and its content. 
+    """
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are making ppt for the class, skilled in making index and its belongings.\
+              showing only the title and content of the slides without other flowery words or summary or etc."},
+            {"role": "user", "content": prompt}
+        ],
+    )
+
+    # get the response content only
+    print(completion.choices[0].message.content)
+    slides = completion.choices[0].message.content.split("Slide")
+    slides = slides[1:]
+
+    slides = ["Slide" + slide.strip() for slide in slides]
+    return slides
+
+
+
+if __name__ == '__main__':
+    topic = "What is the meaning of life?"
+    details = "The meaning of life is to be happy and useful."
+    apikey = "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"
+    print(gpt_pptmaker(topic, details, apikey))
+    
