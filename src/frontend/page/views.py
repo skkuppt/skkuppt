274055a8cfc12a003e01e4aede11adec3ea108d6
yspkm <yskim0830@outlook.com>
2023-11-18 15:48:03 +0000

    fix(): REST API 호출을 환경변수로 뺌

--- src/frontend/page/views.py
+++ src/frontend/page/views.py
@@ -2,18 +2,22 @@ import requests
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import os
 
 @csrf_exempt
 # http:<url>/api/create_ppt
 def create_ppt(request):
+
     if request.method == 'POST':
         # Extract the topic and content from the POST request
         topic = request.POST.get('topic')
         content = request.POST.get('content')
 
         # Send the POST request to the API endpoint
+        BACKEND_HOST=os.environ.get("BACKEND_HOST", "localhost")
+        BACKEND_PORT=os.environ.get("BACKEND_PORT", "8000")
         response = requests.post(
-            'http://localhost:8000/api/question/create',
+            f'http://{BACKEND_HOST}:{BACKEND_PORT}/api/question/create',
             headers={'Content-Type': 'application/json'},
             json={'topic': topic, 'content': content}
         )
--- src/frontend/skkuppt/settings.py
+++ src/frontend/skkuppt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0cx)rk#%oe#(^7+2ddbtui-$)v#+_5!k$qsmvi20l)gkmk-j5*'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", "True"))
+PUBLIC_HOST=os.environ.get("LETSENCRYPT_HOST", "skkuppt.intueri.cloud")
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', PUBLIC_HOST]
 
 
 # Application definition
